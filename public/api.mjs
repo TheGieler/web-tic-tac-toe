6e65290ac6cde123eb797e31c00a33da13a58ac0
TheGieler <giel@raumzeitmedia.de>
2021-05-21 22:46:10 +0200

    Relocate firestore code

---
+++ public/api.mjs
@@ -0,0 +1,16 @@
+// Initialize Cloud Firestore through Firebase
+var firebaseConfig = {
+  apiKey: "AIzaSyBHKE8GChoFYjCJOzSryx6m0JG7JU6Jc_M",
+  authDomain: "webbased-tic-tac-toe.firebaseapp.com",
+  projectId: "webbased-tic-tac-toe",
+  storageBucket: "webbased-tic-tac-toe.appspot.com",
+  messagingSenderId: "446017794661",
+  appId: "1:446017794661:web:c369787ddecbabfad44c8c",
+  measurementId: "G-SDZ9YKPG56",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+
+export { db };
