07907c39f5a8e96536bc11db74c24cc1ac525aeb
TheGieler <giel@raumzeitmedia.de>
2021-05-21 22:44:05 +0200

    Add function printPlayerName

--- public/main.mjs
+++ public/main.mjs
@@ -1,1 +1,8 @@
 import { db } from "./api.mjs";
+
+const printPlayerName = async (docRef) => {
+  const playerId = docRef.id;
+  const playerRef = await db.collection("players").doc(playerId).get();
+  const playerName = playerRef.data().player;
+  document.getElementById("playerName").innerHTML = `You are ${playerName}`;
+};
