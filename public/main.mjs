2b05f580e3f1bd2539b587a5f1c35006da951cd6
TheGieler <giel@raumzeitmedia.de>
2021-05-21 22:44:47 +0200

    Rename main.js to main.mjs

--- public/main.mjs
+++ public/main.mjs
@@ -1,8 +1,24 @@
 import { db } from "./api.mjs";
 
+const main = async () => {
+  const querySnapshot = await db.collection("players").get();
+  const docRef = await db.collection("players").add({
+    player: `Player`,
+    color: "",
+  });
+  printPlayerName(docRef);
+};
+main();
+
 const printPlayerName = async (docRef) => {
   const playerId = docRef.id;
   const playerRef = await db.collection("players").doc(playerId).get();
   const playerName = playerRef.data().player;
   document.getElementById("playerName").innerHTML = `You are ${playerName}`;
 };
+
+// db.collection("players")
+//   .doc("dGr7mwqccimNwnZOWrVx")
+//   .onSnapshot((collectionSnapshot) => {
+//     console.log(collectionSnapshot.data());
+//   });
