6c67e8b74b167cbdf873d97bf4ecf00045ddd4f9
TheGieler <giel@raumzeitmedia.de>
2021-06-16 18:21:07 +0200

    Create collection watcher demo

--- public/main.mjs
+++ public/main.mjs
@@ -8,6 +8,23 @@ const createPlayer = async (num) => {
   return docRef;
 };
 
+db.collection("players").onSnapshot((a) => {
+  const arr = a.docs.map((x) => x);
+  const playerNumber = arr.length + 1;
+  if (arr.find((p) => p.data().player === "Player 1")) {
+    if (arr.find((p) => p.data().player === "Player 2")) {
+      console.log("Please enable the Playboard");
+      return;
+    }
+    console.log("Please create Player 2");
+    createPlayer(playerNumber);
+    return;
+  } else {
+    console.log("Please create Player 1");
+    createPlayer(playerNumber);
+  }
+});
+
 const main = async () => {
   const docRef = await createPlayer();
 };
